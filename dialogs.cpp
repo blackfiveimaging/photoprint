aee4efa0ab8f49bcf4e26e86b6438a5eb6639b6a
Alastair M. Robinson <robinsonb5@gmail.com>
2022-09-02 08:31:22 +0100

    Bumped libraries, added IJG credit to README and About dialog.

--- dialogs.cpp
+++ dialogs.cpp
@@ -404,10 +404,11 @@ void About_Dialog(GtkWindow *parent)
 	
 	GtkWidget *label=gtk_label_new(_("PhotoPrint "
 	PACKAGE_VERSION
-	" - copyright (c) 2004-2011\n"
+	" - copyright (c) 2004-2022\n"
 	"by Alastair M. Robinson (amr@blackfiveservices.co.uk)\nDistributed under the terms\nof the GNU General Public Licence.\n"
 	"See the file 'COPYING' for more details.\n"
-	"The borders pack is released under the Creative Commons\nAttribution 2.0 UK: England & Wales Licence"));
+	"The borders pack is released under the Creative Commons\nAttribution 2.0 UK: England & Wales Licence.\n"
+	"This software is based in part on the work of the Independent JPEG Group."));
 	gtk_label_set_justify(GTK_LABEL(label),GTK_JUSTIFY_CENTER);
 	gtk_box_pack_start(GTK_BOX(GTK_DIALOG(dialog)->vbox),label,TRUE,FALSE,5);
 	gtk_widget_show(label);
